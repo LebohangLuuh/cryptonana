50d4146faefbeb3cd15f90e7a11180a7c07b4a4b
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2025-08-17 15:04:31 +0200

    added vid if no flags show

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,29 +1,41 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { SearchComponent } from '../../ui/search/search.component';
 import { CountryCardComponent } from '../../ui/country-card/country-card.component';
 import { CountryService } from '../../services/country.service';
 import { LoaderService } from '../../services/loader.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  imports: [SearchComponent, CountryCardComponent],
+  imports: [SearchComponent, CountryCardComponent, CommonModule],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent implements OnInit {
   countryService = inject(CountryService);
   loaderService = inject(LoaderService);
 
+  // Signals for UI states
+  hasError = signal(false);
+
   ngOnInit(): void {
     this.loaderService.show();
-    this.countryService.getAllCountries().subscribe(
-      (data) => {
+
+    this.countryService.getAllCountries().subscribe({
+      next: (data) => {
         this.loaderService.hide();
         const countries: any[] = Object.entries(data);
-        this.countryService.setCountries(countries);
+
+        if (countries.length === 0) {
+          this.hasError.set(true);
+        } else {
+          this.countryService.setCountries(countries);
+          this.hasError.set(false);
+        }
       },
-      (error) => {
+      error: () => {
         this.loaderService.hide();
-      }
-    );
+        this.hasError.set(true);
+      },
+    });
   }
 }
